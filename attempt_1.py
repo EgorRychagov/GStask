cc549295bc4719d9dae4a3b76818acd073446e25
EgorRychagov <st076928@student.spbu.ru>
2023-10-13 14:00:14 +0300

    parser added

--- attempt_1.py
+++ attempt_1.py
@@ -21,18 +21,53 @@ class InfoStack:
         print(cls.all_data)
         
     @classmethod
-    def pull(cls, nature, field, _id = "") -> dict:
+    def pull(cls, nature, field = "", _id = "") -> dict:
         if nature not in cls.all_data:
-            return {nature : "not registered"}        
-
+            return {nature : "not registered"}
+        
         if _id == "":
-            return cls.all_data[nature][field]
+            if field in cls.all_data[nature]:
+                return {field : cls.all_data[nature][field]()}
+            else: return {field : "not registered"}
         
         if _id not in cls.all_data:    
             return {_id : "not registered"}
-                
-        return cls.all_data[nature][_id][field]
+        
+        if field in cls.all_data[nature][_id]:    
+            return {field : cls.all_data[nature][_id][field]()}
+        
+        return {field : "not registered"}
         
 
 class Parser:
-    pass
+    def parse(request: dict):
+        response = {}
+        subr_list = {}
+        subresponse = {}
+
+        for nature in request:
+            if nature not in InfoStack.all_data:
+                response.update({nature : "not registered"})
+                continue
+            
+            response.update({nature : {}})
+
+            if "_id" not in request[nature]:
+                for field in request[nature]:
+                    subresponse.update(InfoStack.pull(nature, field))
+                response[nature].update(subresponse.copy())
+                subresponse.clear()
+
+            for _id in request[nature]:
+                if _id not in InfoStack.all_data[nature]:
+                    subr_list.update({_id : "not registered"})
+
+                for field in request[nature][_id]:
+                    subresponse.update(InfoStack.pull(nature, field, _id))
+                subr_list.update({_id : subresponse.copy()})
+                subresponse.clear()
+                
+            response.update({nature : subr_list.copy()})
+            subr_list.clear()
+
+        return response
